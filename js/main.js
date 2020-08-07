b7400efa9d0d0a3907f1cfa2b3f19ea94ea60dba
DeeEssy <venchesters.2002@gmail.com>
2020-08-07 21:53:37 +0300

    da

--- js/main.js
+++ js/main.js
@@ -8,3 +8,78 @@ var instance = new vidbg('.video', {
 var rellax = new Rellax('.rocket');
 
 AOS.init();
+
+const popupMenu = document.querySelector('.popup-menu'),
+    body = document.querySelector('body')
+
+
+let closestItemByClass = function (item, className) {
+    let node = item
+    while (node) {
+        if (node.classList.contains(className)) {
+            return node
+        }
+        node = node.parentElement
+    }
+    return null
+}
+
+let closestAttr = function (item, attr) {
+    let node = item
+    while (node) {
+        let attrValue = node.getAttribute(attr)
+        if (attrValue) {
+            return attrValue
+        }
+        node = node.parentElement
+    }
+    return null
+}
+
+const showPopup = target => {
+    target.classList.add('popup-active')
+}
+
+const closePopup = target => {
+    target.classList.remove('popup-active')
+}
+
+let toggleScroll = () => {
+    body.classList.toggle('no-scroll')
+}
+
+body.addEventListener('click', event => {
+    const target = event.target
+    const popupClass = closestAttr(target, 'data-popup')
+    if (popupClass === null) {
+        return
+    }
+    event.preventDefault()
+    let popup = document.querySelector('.' + popupClass)
+    if (popup) {
+        showPopup(popup)
+        toggleScroll()
+    }
+})
+
+body.addEventListener('click', event => {
+    let target = event.target
+    if (target.classList.contains('popup-close') ||
+        target.classList.contains('popup-inner')) {
+        let popup = closestItemByClass(target, 'popup')
+        closePopup(popup)
+        toggleScroll()
+    }
+})
+
+body.addEventListener('keydown', event => {
+    if (event.keyCode !== 27) {
+        return
+    }
+
+    let popup = document.querySelector('.popup-active')
+    if (popup) {
+        closePopup(popup)
+        toggleScroll()
+    }
+})
